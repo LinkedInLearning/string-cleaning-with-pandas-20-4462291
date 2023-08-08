a5ba5de252fc94ab6c137a9136e1e34daf4334d8
Seth Berry <seth.berry@nd.edu>
2023-08-08 09:54:57 -0700

    code updates for 01_02

--- code/chapter_01/chapter_01_video_02.py
+++ code/chapter_01/chapter_01_video_02.py
@@ -1,12 +1,45 @@
+#####################
+### Testing Speed ###
+#####################
+
 import pandas as pd
 import pyarrow as pa
 import re
 import timeit
 
-small_aliens_numpy = pd.read_csv('small_cosmic.csv')
 
-small_aliens = pd.read_csv('small_cosmic.csv', engine='pyarrow', dtype_backend="pyarrow")
+small_aliens_numpy = pd.read_csv('data/small_cosmic.csv')
 
-aliens_numpy = pd.read_csv("cosmic_data.csv")
+small_aliens = pd.read_csv('data/small_cosmic.csv', engine='pyarrow', dtype_backend='pyarrow')
 
-aliens = pd.read_csv("cosmic_data.csv", engine='pyarrow', dtype_backend="pyarrow")
+aliens_numpy = pd.read_csv('data/cosmic_data.csv')
+
+aliens = pd.read_csv('data/cosmic_data.csv', engine='pyarrow', dtype_backend='pyarrow')
+
+setup_normal = '''
+import pandas as pd
+aliens = pd.read_csv('data/cosmic_data.csv')
+'''
+
+setup_pyarrow = '''
+import pandas as pd
+aliens = pd.read_csv('data/cosmic_data.csv', engine='pyarrow', dtype_backend='pyarrow')
+'''
+
+statement = '''
+aliens['Bio'].str.replace(
+  'and',
+  '&', 
+  regex=True
+)
+'''
+
+tests = {'setup_normal': setup_normal, 'setup_pyarrow': setup_pyarrow}
+
+for key, value in tests.items():
+  times = timeit.repeat(setup=value, 
+                        stmt=statement,
+                        repeat=2,
+                        number=10
+                        )
+  print('The lowest time for ' + key + ': ', min(times))                      
